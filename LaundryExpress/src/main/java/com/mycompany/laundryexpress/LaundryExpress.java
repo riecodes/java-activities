4d7e1fe1f248d36988b6366949a90440c2d40a2a
riecodes <eirmonpaculan11@gmail.com>
2024-12-01 17:52:14 +0800

    added laundry express project

---
+++ LaundryExpress/src/main/java/com/mycompany/laundryexpress/LaundryExpress.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.laundryexpress;
+
+/**
+ *
+ * @author eirmo
+ */
+public class LaundryExpress {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
